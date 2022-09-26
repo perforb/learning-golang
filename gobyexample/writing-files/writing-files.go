b3a1bfc462a823707e429b74559438c4d2132f15
perforb <dev.perfumed.garden@gmail.com>
2022-09-27 01:09:26 +0900

    Add writing-files.go

---
+++ gobyexample/writing-files/writing-files.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// wrote 5 bytes
+// wrote 7 bytes
+// wrote 9 bytes
+func main() {
+	d1 := []byte("hello\ngo\n")
+	err := os.WriteFile("/tmp/dat1", d1, 0644)
+	check(err)
+
+	f, err := os.Create("/tmp/dat2")
+	check(err)
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	// some\n
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+	_ = f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n4)
+	_ = w.Flush()
+}
