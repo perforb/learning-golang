49d228e7fc811d72539cb120dd20c79bdf6af2a5
perforb <dev.perfumed.garden@gmail.com>
2022-09-27 21:06:06 +0900

    Add embed-directive.go

---
+++ gobyexample/embed-directive/embed-directive.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"embed"
+)
+
+//go:embed folder/single_file.txt
+var fileString string
+
+//go:embed folder/single_file.txt
+var fileByte []byte
+
+//go:embed folder/single_file.txt
+//go:embed folder/*.hash
+var folder embed.FS
+
+// hello go
+// hello go
+// 123
+// 456
+func main() {
+	print(fileString)
+	print(string(fileByte))
+
+	content1, _ := folder.ReadFile("folder/file1.hash")
+	print(string(content1))
+	content2, _ := folder.ReadFile("folder/file2.hash")
+	print(string(content2))
+}
