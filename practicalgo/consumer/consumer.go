ab85d95e81e244a495abdda6e2aaf3009a72241d
perforb <dev.perfumed.garden@gmail.com>
2022-11-10 09:49:59 +0900

    Update consumer.go

--- practicalgo/consumer/consumer.go
+++ practicalgo/consumer/consumer.go
@@ -1,15 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Consumer struct {
-	name string
-	age  int
+	name      string
+	age       int
+	expiredAt int64
 }
 
 type Consumers []Consumer
 
-func (c Consumers) YoungConsumer() Consumers {
+func (c Consumers) expires(now time.Time) Consumers {
+	consumers := make([]Consumer, 0, len(c))
+	for _, v := range c {
+		if v.expiredAt < now.UnixMilli() {
+			consumers = append(consumers, v)
+		}
+	}
+	return consumers
+}
+
+func (c Consumers) ExpiredConsumers() Consumers {
+	return c.YoungConsumers().expires(time.Now())
+}
+
+func (c Consumers) YoungConsumers() Consumers {
 	consumers := make([]Consumer, 0, len(c))
 	for _, v := range c {
 		if v.age < 25 {
@@ -22,20 +40,24 @@ func (c Consumers) YoungConsumer() Consumers {
 func main() {
 	consumers := Consumers{
 		Consumer{
-			name: "mike",
-			age:  31,
+			name:      "mike",
+			age:       31,
+			expiredAt: time.Now().Add(1 * time.Minute).UnixMilli(),
 		},
 		Consumer{
-			name: "tom",
-			age:  23,
+			name:      "tom",
+			age:       23,
+			expiredAt: time.Now().Add(-1 * time.Minute).UnixMilli(),
 		},
 		Consumer{
-			name: "ai",
-			age:  17,
+			name:      "ai",
+			age:       17,
+			expiredAt: time.Now().Add(1 * time.Minute).UnixMilli(),
 		},
 	}
 
-	for _, c := range consumers.YoungConsumer() {
-		fmt.Printf("Name: %s, Age: %d\n", c.name, c.age)
+	for _, c := range consumers.ExpiredConsumers() {
+		fmt.Printf("Name: %s, Age: %d, ExpiredAt: %d\n",
+			c.name, c.age, c.expiredAt)
 	}
 }
